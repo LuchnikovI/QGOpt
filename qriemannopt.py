2db2e2199109227c8509d8d815abc25d82654968
LuchnikovI <luchnikovilya@gmail.com>
2020-02-03 14:22:10 +0300

    + Riemannian Adam

--- qriemannopt.py
+++ qriemannopt.py
@@ -45,22 +45,6 @@ def egrad_to_rgrad(u, egrad):
 
 
 @tf.function
-def canonical_metric(v1, v2, u):
-    """Returns g(v1, v2)_u, where g -- canonical metric,
-    v1 and v2 vectors from tangent space and u is a point on
-    a manifold.
-    Args:
-        v1: complex valued tf.Tensor of shape (..., q, p), first vector.
-        v2: complex valued tf.Tensor of shape (..., q, p), second vector.
-        u: complex valued tf.Tensor of shape (..., q, p), points on a manifold
-    Returns:
-        g(v1, v2)_u complex valued tf.Tensor of shape (1,)"""
-
-    ker = tf.eye(u.shape[-2], dtype=u.dtype) - 0.5 * u @ tf.linalg.adjoint(u)
-    return tf.reduce_sum(tf.linalg.trace(tf.linalg.adjoint(v1) @ ker @ v2))
-
-
-@tf.function
 def proj(u, vec):
     """Returns projection of vector on tangen space of Stiefel manifold.
     Args:
@@ -183,5 +167,114 @@ class StiefelSGD(tf.optimizers.Optimizer):
         config.update({
             "learning_rate": self._lr,
             "momentum": self.momentum
+        })
+        return config
+
+
+class StiefelAdam(tf.optimizers.Optimizer):
+
+    def __init__(self,
+               learning_rate=0.001,
+               beta1=0.9,
+               beta2=0.999,
+               eps=1e-8,
+               name="StiefelAdam"):
+        """Constructs a new Adam optimizer on Stiefel
+        manifold.
+        Comment:
+            The StiefelAdam works only with real valued tf.Variable of shape
+            (..., q, p, 2), where ... -- enumerates manifolds 
+            (can be either empty or any shaped),
+            q and p size of an isometric matrix, the last index marks
+            real and imag parts of an isometric matrix
+            (0 -- real part, 1 -- imag part)
+        Args:
+            learning_rate: floating point number. The learning rate.
+            Defaults to 0.001.
+            beta1: floating point number. exp decay rate for first moment.
+            Defaults to 0.9.
+            beta2: floating point number. exp decay rate for second moment.
+            Defaults to 0.999.
+            eps: floating point number. Regularization coeff.
+            Defaults to 1e-8.
+            name: Optional name prefix for the operations created when applying
+            gradients.  Defaults to 'StiefelAdam'."""
+        
+        super(StiefelAdam, self).__init__(name)
+        self.iter = 0
+        self.eps = eps
+        self._lr = learning_rate
+        self._lr_t = self._lr_t = tf.convert_to_tensor(self._lr, name="learning_rate")
+        if isinstance(beta1, (int, float)) and (beta1 < 0 or beta1 > 1):
+            raise ValueError("`beta1` must be between [0, 1].")
+        self.beta1 = beta1
+        if isinstance(beta2, (int, float)) and (beta2 < 0 or beta2 > 1):
+            raise ValueError("`beta2` must be between [0, 1].")
+        self.beta2 = beta2
+
+
+    def _create_slots(self, var_list):
+        # create m and v slots
+        for var in var_list:
+            self.add_slot(var, "m")
+            self.add_slot(var, "v")
+
+
+    def _resource_apply_dense(self, grad, var):
+        
+        self.iter = self.iter + 1
+
+        #Complex version of grad and var
+        complex_var = real_to_complex(var)
+        complex_grad = real_to_complex(grad)
+
+        #tf version of learning rate
+        lr_t = tf.cast(self._lr_t, complex_var.dtype)
+
+        #Riemannian gradient
+        grad_proj = egrad_to_rgrad(complex_var, complex_grad)
+
+        #Complex versions of m and v
+        m = self.get_slot(var, "m")
+        v = self.get_slot(var, "v")
+        m_complex = real_to_complex(m)
+        v_complex = real_to_complex(v)
+        
+        #Update m and v
+        m_complex = self.beta1 * m_complex + (1 - self.beta1) * grad_proj
+        v_complex = self.beta2 * v_complex +\
+        (1 - self.beta2) * tf.cast(tf.math.abs(grad_proj ** 2),
+        dtype=v_complex.dtype)
+        
+        #Bias correction
+        m_corr = m_complex / (1 - self.beta1 ** self.iter)
+        v_corr = v_complex / (1 - self.beta2 ** self.iter)
+
+        #New value of var
+        new_var = complex_var - lr_t * m_corr /\
+        (tf.math.sqrt(v_corr) + self.eps)
+        new_var = retraction(new_var)
+
+        #Vector transport of v and assigning new value of v
+        m_complex = vector_transport(new_var, m_complex)
+        m.assign(complex_to_real(m_complex))
+        
+        #Assigning new value of v
+        v.assign(complex_to_real(v_complex))
+
+        #Update of var
+        var.assign(complex_to_real(new_var))
+
+    def _resource_apply_sparse(self, grad, var):
+        raise NotImplementedError("Sparse gradient updates are not supported.")
+    
+    def get_config(self):
+        config = super(StiefelAdam, self).get_config()
+        config.update({
+            "learning_rate": self._lr,
+            "beta1": self.beta1,
+            "beta2": self.beta2,
+            "eps": self.eps,
+            "iter": self.iter
         })
         return config
