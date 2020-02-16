3923a24fdd268d1819e153b0674d2c7912a830c1
LuchnikovI <luchnikovilya@gmail.com>
2020-02-16 18:47:16 +0300

    + canonical metric for Stiefel

--- qriemannopt/manifold/stiefel.py
+++ qriemannopt/manifold/stiefel.py
@@ -5,6 +5,7 @@ class StiefelManifold(base_manifold.Manifold):
     """Class is used to work with Stiefel manifold. It allows performing all
     necessary operations with elements of manifolds direct product and
     tangent spaces for optimization."""
+    # TODO check correctness of transport for canonical metric
     
     def __init__(self, retraction='svd',
                  metric='euclidean',
@@ -15,13 +16,13 @@ class StiefelManifold(base_manifold.Manifold):
             'svd'. Types of retraction is available now: 'svd', 'cayley'.
             
             metric: string specifies type of metric, Defaults to 'euclidean'.
-            Types of metrics is available now: 'euclidean'.
+            Types of metrics is available now: 'euclidean', 'canonical'.
             
             transport: string specifies type of vector transport,
             Defaults to 'projective'. Types of vector transport
             is available now: 'projective'."""
         
-        list_of_metrics = ['euclidean']
+        list_of_metrics = ['euclidean', 'canonical']
         list_of_retractions = ['svd', 'cayley']
         list_of_transports = ['projective']
         
@@ -50,7 +51,13 @@ class StiefelManifold(base_manifold.Manifold):
             complex valued tensor of shape (...,),
             manifold wise inner product"""
         if self._metric=='euclidean':
-            s_sq = tf.linalg.trace(vec1 @ tf.linalg.adjoint(vec2))[...,
+            s_sq = tf.linalg.trace(tf.linalg.adjoint(vec1) @ vec2)[...,
+                                  tf.newaxis,
+                                  tf.newaxis]
+            
+        elif self._metric=='canonical':
+            G = tf.eye(u.shape[-2], dtype=u.dtype) - u @ tf.linalg.adjoint(u) / 2
+            s_sq = tf.linalg.trace(tf.linalg.adjoint(vec1) @ G @ vec2)[...,
                                   tf.newaxis,
                                   tf.newaxis]
         return tf.sqrt(s_sq)
@@ -68,11 +75,10 @@ class StiefelManifold(base_manifold.Manifold):
         Returns:
             complex valued tf.Tensor of shape (..., q, p), projected vector"""
             
-        if self._metric=='euclidean':
-            return 0.5 * u @ (tf.linalg.adjoint(u) @ vec -\
-                              tf.linalg.adjoint(vec) @ u) +\
-            (tf.eye(u.shape[-2], dtype=u.dtype) -\
-             u @ tf.linalg.adjoint(u)) @ vec
+        return 0.5 * u @ (tf.linalg.adjoint(u) @ vec -\
+                          tf.linalg.adjoint(vec) @ u) +\
+        (tf.eye(u.shape[-2], dtype=u.dtype) -\
+         u @ tf.linalg.adjoint(u)) @ vec
         
     
     @tf.function
@@ -91,6 +97,9 @@ class StiefelManifold(base_manifold.Manifold):
                               tf.linalg.adjoint(egrad) @ u) +\
             (tf.eye(u.shape[-2], dtype=u.dtype) -\
              u @ tf.linalg.adjoint(u)) @ egrad
+             
+        elif self._metric=='canonical':
+            return egrad - u @ tf.linalg.adjoint(egrad) @ u
              
     
     @tf.function
