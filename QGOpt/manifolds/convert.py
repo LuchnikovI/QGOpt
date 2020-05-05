55f57348eec16389df6e7aeefbde82a75afd30bc
LuchnikovI <luchnikovilya@gmail.com>
2020-05-06 00:48:30 +0300

    corrections of docstrings

--- QGOpt/manifolds/convert.py
+++ QGOpt/manifolds/convert.py
@@ -2,9 +2,9 @@ import tensorflow as tf
 
 
 def complex_to_real(tensor):
-    """Returns tensor converted from complex tensor of shape
-    (...,) to real tensor of shape (..., 2), where last index
-    marks real [0] and imag [1] parts of complex valued tensor.
+    """Returns tensor converted from a complex type of shape
+    (...,) to a real type of shape (..., 2), where last index
+    marks real [0] and imag [1] parts of a complex valued tensor.
     Args:
         tensor: complex valued tf.Tensor of shape (...,)
     Returns:
@@ -14,10 +14,10 @@ def complex_to_real(tensor):
 
 
 def real_to_complex(tensor):
-    """Returns tensor converted from real tensor of shape
-    (..., 2) to complex tensor of shape (...,), where last index
-    of real tensor marks real [0] and imag [1]
-    parts of complex valued tensor.
+    """Returns tensor converted from a real type of shape
+    (..., 2) to complex type of shape (...,), where last index
+    of a real tensor marks real [0] and imag [1]
+    parts of a complex valued tensor.
     Args:
         tensor: real valued tf.Tensor of shape (..., 2)
     Returns:
