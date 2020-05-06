1667bd7e06befe3b6b938eaa5882bc547a4c6afa
LuchnikovI <luchnikovilya@gmail.com>
2020-05-06 12:42:19 +0300

    Update __init__.py

--- QGOpt/optimizers/__init__.py
+++ QGOpt/optimizers/__init__.py
@@ -1,3 +1,8 @@
 """Riemannian first-order optimizers"""
 from QGOpt.optimizers.SGD import RSGD
 from QGOpt.optimizers.Adam import RAdam
+
+_directly_imported = ['SGD', 'Adam']
+
+__all__ = [s for s in dir() if
+           s not in _directly_imported and not s.startswith('_')]
