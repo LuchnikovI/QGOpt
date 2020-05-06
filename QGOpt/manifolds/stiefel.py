07b1745d3ea3d01066d8e566b526a163689b36fb
LuchnikovI <luchnikovilya@gmail.com>
2020-05-06 15:13:55 +0300

    Update stiefel.py

--- QGOpt/manifolds/stiefel.py
+++ QGOpt/manifolds/stiefel.py
@@ -15,22 +15,21 @@ def adj(A):
 class StiefelManifold(base_manifold.Manifold):
     """Class describes Stiefel manifold. It allows performing all
     necessary operations with elements of manifolds direct product and
-    tangent spaces for optimization."""
+    tangent spaces for optimization. Returns object of class StiefelManifold.
+    Args:
+        retraction: string specifies type of retraction. Defaults to
+        'svd'. Types of retraction is available now: 'svd', 'cayley'.
+
+        metric: string specifies type of metric, Defaults to 'euclidean'.
+        Types of metrics is available now: 'euclidean', 'canonical'.
+
+        transport: string specifies type of vector transport,
+        Defaults to 'projective'. Types of vector transport
+        is available now: 'projective'."""
 
     def __init__(self, retraction='svd',
                  metric='euclidean',
                  transport='projective'):
-        """Returns object of class StiefelManifold.
-        Args:
-            retraction: string specifies type of retraction. Defaults to
-            'svd'. Types of retraction is available now: 'svd', 'cayley'.
-
-            metric: string specifies type of metric, Defaults to 'euclidean'.
-            Types of metrics is available now: 'euclidean', 'canonical'.
-
-            transport: string specifies type of vector transport,
-            Defaults to 'projective'. Types of vector transport
-            is available now: 'projective'."""
 
         list_of_metrics = ['euclidean', 'canonical']
         list_of_retractions = ['svd', 'cayley']
