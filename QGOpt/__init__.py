b0c51aa9abcb3ef14890b1f18e3769d9b71ae58c
LuchnikovI <luchnikovilya@gmail.com>
2020-05-06 12:31:41 +0300

    Update __init__.py

--- QGOpt/__init__.py
+++ QGOpt/__init__.py
@@ -2,3 +2,8 @@
 on manifolds which friquently appear in the quantum data processing"""
 from QGOpt import manifolds
 from QGOpt import optimizers
+
+_directly_imported = ['base_manifold', 'convert', 'densm(old)', 'positivecone', 'stiefel', 'Adam', 'SGD']
+
+__all__ = [s for s in dir() if
+           s not in _directly_imported and not s.startswith('_')]
