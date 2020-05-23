be8634ff6640caa888e71806f08c8ceeea4fb094
LuchnikovI <luchnikovilya@gmail.com>
2020-05-23 18:19:45 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
      name='QGOpt',  
-     version='0.11',
+     version='0.2-alpha',
      author="I. Luchnikov, M. Krechetov",
      author_email="luchnikovilya@gmail.com",
      description="Riemannian optimization for quantum mechanics",
@@ -11,7 +11,7 @@ setuptools.setup(
      packages=setuptools.find_packages(),
      classifiers=[
          "Programming Language :: Python :: 3",
-         "License :: OSI Approved :: MIT License",
+         "License :: OSI Approved :: Apache License 2.0",
          "Operating System :: OS Independent",
      ],
  )
