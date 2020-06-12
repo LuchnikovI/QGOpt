4586dd27efafb09816c5e91c124ec04daf778cec
LuchnikovI <luchnikovilya@gmail.com>
2020-06-12 12:38:02 +0300

    Update stiefel.py

--- QGOpt/manifolds/stiefel.py
+++ QGOpt/manifolds/stiefel.py
@@ -4,29 +4,27 @@ from QGOpt.manifolds.utils import adj
 
 
 class StiefelManifold(base_manifold.Manifold):
-    """The class provides tools for moving points and vectors along the
-    complex Stiefel manifold (St(n, p) -- the manifold of complex valued
-    isometric matrices of size nxp) and along a direct product of several
-    Stiefel manifolds. The geometry of the complex Stiefel manifold is taken
-    from
-    -----------------------------------------------------------------------
+    """The complex Stiefel manifold (St(n, p) -- the manifold of complex
+    valued isometric matrices of size nxp). One can use it to perform
+    moving of points and vectors along the manifold.
+
+    The geometry of the complex Stiefel manifold is taken from
+
     Sato, H., & Iwai, T. (2013, December). A complex singular value 
     decomposition algorithm based on the Riemannian Newton method.
     In 52nd IEEE Conference on Decision and Control (pp. 2972-2978). IEEE.
-    -----------------------------------------------------------------------
-    Another paper, which was used as a guide for implementation of this
-    class is
-    -----------------------------------------------------------------------
+
+    Another paper, which was used as a guide is
+
     Edelman, A., Arias, T. A., & Smith, S. T. (1998). The geometry of
     algorithms with orthogonality constraints. SIAM journal on Matrix
     Analysis and Applications, 20(2), 303-353.
-    -----------------------------------------------------------------------
+
     Args:
         retraction: string specifies type of retraction. Defaults to
-        'svd'. Types of retraction are available: 'svd', 'cayley', 'qr'.
-
+            'svd'. Types of retraction are available: 'svd', 'cayley', 'qr'.
         metric: string specifies type of metric, Defaults to 'euclidean'.
-        Types of metrics are available: 'euclidean', 'canonical'."""
+            Types of metrics are available: 'euclidean', 'canonical'."""
 
     def __init__(self, retraction='svd',
                  metric='euclidean'):
