0ebf3556edcd1a2732cd23ae0100ed21aab93b91
LuchnikovI <luchnikovilya@gmail.com>
2020-06-14 02:16:16 +0300

    Update choimatrix.py

--- QGOpt/manifolds/choimatrix.py
+++ QGOpt/manifolds/choimatrix.py
@@ -7,16 +7,16 @@ import math
 
 class ChoiMatrix(base_manifold.Manifold):
     """The manifold of Choi matrices of fixed rank (Kraus rank).
-    Choi matrices of fixed Kraus rank are the set of matrices Choi(n, k)
-    of size n^2xn^2 (n is the dimension of a quantum system) and rank k
+    Choi matrices of fixed Kraus rank are the set of matrices of
+    size (n^2)x(n^2) (n is the dimension of a quantum system) with rank k
     that are positive definite (the corresponding quantum channel is
-    completely positive) and with additional constraint Tr_2(choi) = Id,
+    completely positive) and with the additional constraint Tr_2(choi) = Id,
     where Tr_2 is the partial trace over the second subsystem, Id is
     the identity matrix (ensures the trace-preserving property of the
     corresponding quantum channel). In the general case Kraus rank of
-    a Choi matrix is equal to n^2. Choi matrices are parametr. An element
-    of this manifold is represented by a complex matrix A of size n^2xk that
-    parametrizes a Choi matrix choi = A @ adj(A) (positive by construction).
+    a Choi matrix is equal to n^2. An element of this manifold is 
+    represented by a complex matrix A of size (n^2)xk that parametrizes
+    a Choi matrix choi = A @ adj(A) (positive by construction).
     Notice that for any unitary matrix Q of size kxk the transformation
     A --> AQ leaves resulting matrix the same. This fact is taken into
     account by consideration of quotient manifold from
@@ -33,7 +33,11 @@ class ChoiMatrix(base_manifold.Manifold):
         All methods of this class operates with tensors of shape (..., n ** 2, k),
         where (...) enumerates manifold (can be any shaped), (n ** 2, k)
         is the shape of a particular matrix (e.g. an element of the manifold
-        or its tangent vector)."""
+        or its tangent vector).
+        In order to take a partial trace of a choi matrix over the second
+        subsystem one can at first reshape a choi matrix 
+        (n ** 2, n ** 2) --> (n, n, n, n) and then take a trace over
+        1st and 3rd indices (the numeration starts from 0)."""
 
     def __init__(self, metric='euclidean'):
 
@@ -43,47 +47,54 @@ class ChoiMatrix(base_manifold.Manifold):
             raise ValueError("Incorrect metric")
 
     def inner(self, u, vec1, vec2):
-        """Returns scalar product of vectors in the tangent space at a
-        point u of manifolds direct product.
+        """Returns manifold wise inner product of vectors from
+        a tangent space.
+
         Args:
-            u: point where a tangent space is considered.
-            vec1: complex valued tensor of shape (..., n ** 2, n ** 2),
-            the first vector from tangent space.
-            vec2: complex valued tensor of shape (..., n ** 2, n ** 2),
-            the second vector from tangent space.
+            u: complex valued tensor of shape (..., n ** 2, k),
+                a set of points from the manifold.
+            vec1: complex valued tensor of shape (..., n ** 2, k),
+                a set of tangent vectors from the manifold.
+            vec2: complex valued tensor of shape (..., n ** 2, k),
+                a set of tangent vectors from the manifold.
+
         Returns:
-            complex valued tensor of shape (...),
-            manifold wise inner product"""
+            complex valued tensor of shape (..., 1, 1),
+                manifold wise inner product"""
+
         prod = tf.reduce_sum(tf.math.conj(vec1) * vec2, axis=(-2, -1))
         prod = tf.math.real(prod)
-        prod = tf.cast(prod, dtype=u.dtype)
+        prod = tf.cast(prod, dtype=u.dtype)[..., tf.newaxis, tf.newaxis]
         return prod
 
     def proj(self, u, vec):
-        """Returns projection of a vector on a tangen spaces
-        of manifolds direct product.
-        Args:
-            u: complex valued tensor of shape (..., n ** 2, n ** 2),
-            point of a direct product of manifolds
-            vec: complex valued tensor of shape (..., n ** 2, n ** 2),
-            vector to be projected
-        Returns:
-            complex valued tensor of shape (..., n ** 2, n ** 2),
-            projected vector"""
+        """Returns projection of vectors on a tangen space
+        of the manifold.
 
-        dim = int(math.sqrt(u.shape[-1]))
+        Args:
+            u: complex valued tensor of shape (..., n ** 2, k),
+                a set of points from the manifold.
+            vec: complex valued tensor of shape (..., n ** 2, k),
+                a set of vectors to be projected.
+
+        Returns:
+            complex valued tensor of shape (..., n ** 2, k),
+            a set of projected vectors."""
+
+        k = u.shape[-1]
+        n = int(math.sqrt(u.shape[-2]))
         shape = u.shape[:-2]
 
-        # projection onto the tangent space of Stiefel manifold
-        vec_mod = tf.reshape(vec, shape + (dim, dim ** 3))
+        # projection onto the tangent space of the Stiefel manifold
+        vec_mod = tf.reshape(vec, shape + (n, k * n))
         vec_mod = tf.linalg.matrix_transpose(vec_mod)
 
-        u_mod = tf.reshape(u, shape + (dim, dim ** 3))
+        u_mod = tf.reshape(u, shape + (n, k * n))
         u_mod = tf.linalg.matrix_transpose(u_mod)
         vec_mod = vec_mod - 0.5 * u_mod @ (adj(u_mod) @ vec_mod +\
                                            adj(vec_mod) @ u_mod)
         vec_mod = tf.linalg.matrix_transpose(vec_mod)
-        vec_mod = tf.reshape(vec_mod, shape + (dim ** 2, dim ** 2))
+        vec_mod = tf.reshape(vec_mod, shape + (n ** 2, k))
 
         # projection onto the horizontal space
         uu = adj(u) @ u
@@ -92,82 +103,127 @@ class ChoiMatrix(base_manifold.Manifold):
 
     def egrad_to_rgrad(self, u, egrad):
         """Returns the Riemannian gradient from an Euclidean gradient.
-        Args:
-            u: complex valued tensor of shape (..., n ** 2, n ** 2),
-            point from manifolds direct product.
-            egrad: complex valued tensor of shape (..., n ** 2, n ** 2),
-            Eucledian gradient
-        Returns:
-            complex valued tensor of shape (..., n ** 2, n ** 2)."""
 
-        dim = int(math.sqrt(u.shape[-1]))
+        Args:
+            u: complex valued tensor of shape (..., n ** 2, k),
+                a set of points from the manifold.
+            egrad: complex valued tensor of shape (..., n ** 2, k),
+                a set of Euclidean gradients.
+
+        Returns:
+            complex valued tensor of shape (..., n ** 2, k),
+            the set of Reimannian gradients."""
+
+        k = u.shape[-1]
+        n = int(math.sqrt(u.shape[-2]))
         shape = u.shape[:-2]
 
-        # projection onto the tangent space of Stiefel manifold
-        vec_mod = tf.reshape(egrad, shape + (dim, dim ** 3))
+        # projection onto the tangent space of the Stiefel manifold
+        vec_mod = tf.reshape(egrad, shape + (n, k * n))
         vec_mod = tf.linalg.matrix_transpose(vec_mod)
 
-        u_mod = tf.reshape(u, shape + (dim, dim ** 3))
+        u_mod = tf.reshape(u, shape + (n, k * n))
         u_mod = tf.linalg.matrix_transpose(u_mod)
         vec_mod = vec_mod - 0.5 * u_mod @ (adj(u_mod) @ vec_mod +\
                                            adj(vec_mod) @ u_mod)
         vec_mod = tf.linalg.matrix_transpose(vec_mod)
-        vec_mod = tf.reshape(vec_mod, shape + (dim ** 2, dim ** 2))
+        vec_mod = tf.reshape(vec_mod, shape + (n ** 2, n ** 2))
         return vec_mod
 
     def retraction(self, u, vec):
-        """Transports point according a retraction map.
-        Args:
-            u: complex valued tensor of shape (..., n ** 2, n ** 2),
-            a point to be transported.
-            vec: complex valued tensor of shape (..., n ** 2, n ** 2),
-            a direction vector.
-        Returns:
-            complex valued tensor of shape (..., n ** 2, n ** 2),
-            a new point"""
+        """Transports a set of points from the manifold via a
+        retraction map.
 
-        dim = int(math.sqrt(u.shape[-1]))
+        Args:
+            u: complex valued tensor of shape (..., n ** 2, k), a set
+                of points to be transported.
+            vec: complex valued tensor of shape (..., n ** 2, k),
+                a set of direction vectors.
+
+        Returns:
+            complex valued tensor of shape (..., n ** 2, k),
+            a set of transported points."""
+
+        k = u.shape[-1]
+        n = int(math.sqrt(u.shape[-2]))
         shape = u.shape[:-2]
 
         # svd based retraction
         u_new = u + vec
-        u_new = tf.reshape(u_new, shape + (dim, dim ** 3))
+        u_new = tf.reshape(u_new, shape + (n, n * k))
         _, U, V = tf.linalg.svd(u_new)
 
         u_new = U @ adj(V)
-        u_new = tf.reshape(u_new, shape + (dim ** 2, dim ** 2))
+        u_new = tf.reshape(u_new, shape + (n ** 2, n ** 2))
         return u_new
 
     def vector_transport(self, u, vec1, vec2):
-        """Returns vector vec1 tranported from point u along vector vec2.
+        """Returns a vector tranported along an another vector
+        via vector transport.
+
         Args:
-            u: complex valued tensor of shape (..., n ** 2, n ** 2),
-            an initial point of a manifolds
-            direct product
-            vec1: complex valued tensor of shape (..., n ** 2, n ** 2),
-            a vector to be transported
-            vec2: complex valued tensor of shape (..., n ** 2, n ** 2),
-            a direction vector.
+            u: complex valued tensor of shape (..., n ** 2, k),
+                a set of points from the manifold, starting points.
+            vec1: complex valued tensor of shape (..., n ** 2, k),
+                a set of vectors to be transported.
+            vec2: complex valued tensor of shape (..., n ** 2, k),
+                a set of direction vectors.
+
         Returns:
-            complex valued tensor of shape (..., n ** 2, n ** 2),
-            a new vector"""
+            complex valued tensor of shape (..., n ** 2, k),
+            a set of transported vectors."""
 
         u_new = self.retraction(u, vec2)
         return self.proj(u_new, vec1)
 
     def retraction_transport(self, u, vec1, vec2):
         """Performs a retraction and a vector transport simultaneously.
+
         Args:
-            u: complex valued tensor of shape (..., p, p),
-            an initial point of a manifolds
-            direct product
-            vec1: complex valued tensor of shape (..., p, p),
-            a vector to be transported
-            vec2: complex valued tensor of shape (..., p, p),
-            a direction vector.
+            u: complex valued tensor of shape (..., n ** 2, k),
+                a set of points from the manifold, starting points.
+            vec1: complex valued tensor of shape (..., n ** 2, k),
+                a set of vectors to be transported.
+            vec2: complex valued tensor of shape (..., n ** 2, k),
+                a set of direction vectors.
+
         Returns:
-            two complex valued tensors of shape (..., p, p),
-            a new point and a transported vector"""
+            two complex valued tensors of shape (..., n ** 2, k),
+            a set of transported points and vectors."""
 
         u_new = self.retraction(u, vec2)
         return u_new, self.proj(u_new, vec1)
+
+    def random(self, shape, dtype=tf.complex64):
+        """Returns a set of points from the manifold generated
+        randomly.
+
+        Args:
+            shape: tuple of integer numbers (..., n ** 2, k),
+                shape of a generated matrix.
+            dtype: type of an output tensor, can be
+                either tf.complex64 or tf.complex128.
+
+        Returns:
+            complex valued tensor of shape (..., n ** 2, k),
+            a generated matrix."""
+
+        list_of_dtypes = [tf.complex64, tf.complex128]
+
+        if dtype not in list_of_dtypes:
+            raise ValueError("Incorrect dtype")
+        real_dtype = tf.float64 if dtype == tf.complex128 else tf.float32
+
+        u = tf.complex(tf.random.normal(shape, dtype=real_dtype),
+                       tf.random.normal(shape, dtype=real_dtype))
+
+        k = shape[-1]
+        n = int(math.sqrt(shape[-2]))
+
+        u = tf.reshape(u, shape[:-2] + (n, n * k))
+        u = tf.linalg.matrix_transpose(u)
+        u, _ = tf.linalg.qr(u)
+        u = tf.linalg.matrix_transpose(u)
+        u = tf.reshape(u, shape[:-2] + (n ** 2, k))
+
+        return u
