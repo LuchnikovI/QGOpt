9b42cdd2714bd0c079b094f154391a32ff8102bc
RyAlAl <alexruzhov@gmail.com>
2021-01-24 15:55:27 +0300

    Update test_optimizers.py

--- tests/test_optimizers.py
+++ tests/test_optimizers.py
@@ -52,4 +52,4 @@ err_dict = {}
 for key, opt in opts.items():
     loss = optimize(q, h, number_of_steps, opt)
     loss = tf.math.abs(loss - exact_solution)
-    assert loss < 1.0e-8, "Optimizer error for"
+    assert loss < 1.0e-6, "Optimizer error for {}.".format(key)
